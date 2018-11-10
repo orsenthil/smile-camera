8c849e225846311c24702920e9fce26ea6f3518e
Senthil Kumaran <skumaran@mesosphere.io>
2018-11-10 15:50:44 -0800

    Updated smile.

--- smile.py
+++ smile.py
@@ -21,7 +21,9 @@ def smile(mouth):
 COUNTER = 0
 TOTAL = 0
 
-shape_predictor= "shape_predictor_68_face_landmarks.dat"
+SMILE_THRESHOLD = 0.30
+
+shape_predictor = "shape_predictor_68_face_landmarks.dat"
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(shape_predictor)
@@ -37,53 +39,71 @@ time.sleep(1.0)
 
 fps = FPS().start()
 
-cv2.namedWindow("smile dectector.")
+cv2.namedWindow("smile detector.")
 
 while True:
     frame = vs.read()
 
     frame = imutils.resize(frame, width=450)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    rects = detector(gray, 0)
-    print(len(rects))
+    face_rects = detector(grayscale_frame, 0)
+    num_faces = len(face_rects)
 
-    for rect in rects:
-        shape = predictor(gray, rect)
+    face_smile_values = {face_id: 0 for face_id in range(num_faces)}
+
+    for idx, face in enumerate(face_rects):
+        shape = predictor(grayscale_frame, face)
         shape = face_utils.shape_to_np(shape)
         mouth = shape[mStart:mEnd]
+
         mar = smile(mouth)
+        face_smile_values[idx] = mar
         mouthHull = cv2.convexHull(mouth)
-        #print(shape)
 
         cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
 
-        if mar > .58 :
-            COUNTER += 1
-        else:
-            if COUNTER >= 15:
-                TOTAL += 1
-                frame = vs.read()
-                time.sleep(.3)
-                frame2= frame.copy()
-                img_name = "smile_photo_{}.png".format(TOTAL)
-                cv2.imwrite(img_name, frame)
-                print("{} written!".format(img_name))
-            COUNTER = 0
+        sum_of_smiles = sum(face_smile_values.values())
+        total_smile_threshold = SMILE_THRESHOLD * num_faces
 
-        cv2.putText(frame, "MAR: {}".format(mar), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        if sum_of_smiles > total_smile_threshold:
+            COUNTER += 1
+
+        else:
+            if COUNTER >= 10:
+                TOTAL += 1
+                time.sleep(0.2)
+                cv2.putText(frame, "Click!", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+                frame = vs.read()
+
+                frame2 = frame.copy()
+
+                img_name = "smile_photo_{}.png".format(TOTAL)
+
+                cv2.imwrite(img_name, frame2)
+                print("{} written!".format(img_name))
+
+            else:
+                if COUNTER < 3:
+                    cv2.putText(frame, "Smile!", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 255), 2)
+                else:
+                    cv2.putText(frame, "Smile Wider!", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 0), 2)
+
+
+            COUNTER = 0
 
     cv2.imshow("Smile Camera", frame)
 
     fps.update()
 
     key2 = cv2.waitKey(1) & 0xFF
+
     if key2 == ord('q'):
         break
 
 fps.stop()
-
 
 cv2.destroyAllWindows()
 vs.stop()
