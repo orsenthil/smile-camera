0257ae00b25d973dcf8ced2c394382db4e493ec9
Senthil Kumaran <senthil@uthcode.com>
2019-11-03 06:03:09 -0800

    Smile Camera.

--- smile.py
+++ smile.py
@@ -2,8 +2,6 @@ import subprocess
 from scipy.spatial import distance as dist
 from imutils.video import VideoStream, FPS
 from imutils import face_utils
-import imutils
-import numpy as np
 import time
 import dlib
 import cv2
@@ -31,11 +29,11 @@ def get_unique_timestamp():
     return _now.strftime("%Y-%m-%d-%H-%M-%S")
 
 
-SMILE_THRESHOLD = 0.20
+SMILE_THRESHOLD = 0.30
 
 
 def click():
-    subprocess.call(["afplay", "/Users/senthil/facedetect/click.wav"])
+    subprocess.call(["aplay", "click.wav"])
 
 
 shape_predictor = "shape_predictor_68_face_landmarks.dat"
@@ -49,7 +47,7 @@ print("[INFO] starting video stream thread...")
 
 # 640, 480 can be tried too.
 # , resolution=(800, 600)
-vs = VideoStream(src=0).start()
+vs = VideoStream(src=0, resolution=(640, 480)).start()
 fileStream = False
 
 time.sleep(1.0)
@@ -81,7 +79,7 @@ while True:
         face_smile_values[idx] = mar
         mouthHull = cv2.convexHull(mouth)
 
-        #cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
 
         sum_of_smiles = sum(face_smile_values.values())
         total_smile_threshold = SMILE_THRESHOLD * num_faces
@@ -89,30 +87,28 @@ while True:
         if sum_of_smiles > total_smile_threshold:
             COUNTER += 1
 
-        else:
-            if COUNTER >= 3:
-                TOTAL += 1
-                time.sleep(0.2)
-                click()
+        if COUNTER >= 6:
+            TOTAL += 1
+            time.sleep(0.2)
+            click()
 
-                cv2.putText(frame, "Click!", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(frame, "Click!", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
-                frame = vs.read()
+            frame = vs.read()
 
-                frame2 = frame.copy()
+            frame2 = frame.copy()
 
-                img_name = "smile_photo_{total}_{ts}.png".format(total=TOTAL, ts=get_unique_timestamp())
+            img_name = "smile_photo_{total}_{ts}.png".format(total=TOTAL, ts=get_unique_timestamp())
 
-                cv2.imwrite(img_name, frame2)
-                print("{} written!".format(img_name))
-
-            else:
-                if COUNTER < 5:
-                    cv2.putText(frame, "Smile wide!", (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 2)
-                else:
-                    cv2.putText(frame, "Smile Wider!", (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 0), 2)
-
+            cv2.imwrite(img_name, frame2)
+            print("{} written!".format(img_name))
             COUNTER = 0
+
+        else:
+            if COUNTER < 5:
+                cv2.putText(frame, "Smile wide!", (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 2)
+            else:
+                cv2.putText(frame, "Smile Wider!", (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 0), 2)
 
     cv2.imshow(window_name, frame)
 
